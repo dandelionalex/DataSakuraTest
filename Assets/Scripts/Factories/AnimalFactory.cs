ffd88fcf3e5e2059a1dbdf8ef1a9e04e781ba7ca
dandelionalex <104620766+dandelionalex@users.noreply.github.com>
2025-09-29 22:14:17 +0300

    forget file

--- Assets/Scripts/Factories/AnimalFactory.cs
+++ Assets/Scripts/Factories/AnimalFactory.cs
@@ -2,34 +2,43 @@ using UnityEngine;
 using Zenject;
 using Zoo.Config;
 using Zoo.View;
+using UnityEngine.Pool;
 
 namespace Zoo.Factories
 {
     public sealed class AnimalFactory
     {
-        DiContainer         _container;
-        UIInWorldFactory    _UIInWorldFactory;
+        DiContainer _container;
+        UIInWorldFactory _UIInWorldFactory;
+        IObjectPool<GameObject> _animalsPool;
 
-        public AnimalFactory( DiContainer container, UIInWorldFactory UIInWorldFactory )
+        public AnimalFactory(DiContainer container, UIInWorldFactory UIInWorldFactory)
         {
             _container = container;
             _UIInWorldFactory = UIInWorldFactory;
+            _animalsPool = new ObjectPool<GameObject>();
         }
 
-        public IAnimalPresenter Spawn( AnimalConfig animalConfig, Vector3 position, Transform parent )
+        public IAnimalPresenter Spawn(AnimalConfig animalConfig, Vector3 position, Transform parent)
         {
-            var view = _container.InstantiatePrefab( animalConfig.prefab, position, Quaternion.identity, parent );
+            var view = _container.InstantiatePrefab(animalConfig.prefab, position, Quaternion.identity, parent);
             var animalView = view.GetComponent<AnimalView>();
 
-            if ( animalView == null )
+            if (animalView == null)
             {
                 throw new System.Exception("an AnimalView script must be attached to animal prefab");
             }
 
-            var animalPresenter = new AnimalPresenter( animalView, animalConfig.MovementBehaviour, animalConfig.animalType, _UIInWorldFactory );
+            var animalPresenter = new AnimalPresenter(animalView, animalConfig.MovementBehaviour, animalConfig.animalType, _UIInWorldFactory);
             animalView.AnimalPresenter = animalPresenter;
 
             return animalPresenter;
+        }
+
+        AnimalConfig _animalConfig;
+        GameObject CreateGameObject()
+        {
+            var view = _container.InstantiatePrefab(_animalConfig.prefab); 
         }
     }
 }
